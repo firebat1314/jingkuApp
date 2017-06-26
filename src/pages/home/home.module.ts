1dd73087a55f9359e9bb33f636805bd5284c80af
firebat1314 <1121112856@qq.com>
2017-06-26 11:58:39 +0800

    添加,schemas：[CUSTOM_ELEMENTS_SCHEMA]

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HomePage } from './home';
 import { IonicPageModule } from 'ionic-angular';
 
 @NgModule({
   declarations: [HomePage],
   imports: [IonicPageModule.forChild(HomePage)],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule { }
