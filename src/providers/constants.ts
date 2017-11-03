0882d70689f8b82b47a4c14ef561ebf247bb2bc1
firebat1314 <1121112856@qq.com>
2017-11-03 20:53:57 +0800

    版本

--- src/providers/constants.ts
+++ src/providers/constants.ts
@@ -5,6 +5,7 @@ export const IOS_DOWNLOAD = 'https://itunes.apple.com/cn/app/%E9%95%9C%E5%BA%93/
 // export const IP = 'http://v403app.jingkoo.net';//app测试
 export const IP = 'http://app.jingku.cn';//app正式
 // export const IP = 'http://newm.jingkoo.net';//m站
+// export const IP = 'http://m.jingku.cn';//m正站
 // export const IP = 'http://hl.jingkoo.net/';//响应速度测试
 // export const IP = 'http://newpc.jingkoo.net';//pc站
 
