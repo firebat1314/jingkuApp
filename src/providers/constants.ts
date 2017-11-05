c771c37e5cad3a23454ad2dff05463e56cd3492c
firebat1314 <1121112856@qq.com>
2017-11-05 17:51:39 +0800

    11.5 2

--- src/providers/constants.ts
+++ src/providers/constants.ts
@@ -3,11 +3,14 @@ export const APK_DOWNLOAD = 'https://download.fir.im/apps/5922bcefca87a836ff0009
 export const IOS_DOWNLOAD = 'https://itunes.apple.com/cn/app/%E9%95%9C%E5%BA%93/id1265021927?mt=8';
 
 // export const IP = 'http://v403app.jingkoo.net';//app测试
-// export const IP = 'http://app.jingku.cn';//app正式
 // export const IP = 'http://newm.jingkoo.net';//m站
-export const IP = 'http://m.jingku.cn';//m正站
 // export const IP = 'http://hl.jingkoo.net/';//响应速度测试
 // export const IP = 'http://newpc.jingkoo.net';//pc站
+
+
+// export const IP = 'http://app.jingku.cn';
+export const IP = 'http://m.jingku.cn';
+// export const IP = 'http://newapp.jingkoo.net';
 
 export const welpage_selected = '';
 
