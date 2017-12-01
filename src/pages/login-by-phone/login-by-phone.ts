f3081b0bd3bf8e98c489c062f1ccf03346650559
firebat1314 <1121112856@qq.com>
2017-12-01 18:26:50 +0800

    订单球柱镜、支付

--- src/pages/login-by-phone/login-by-phone.ts
+++ src/pages/login-by-phone/login-by-phone.ts
@@ -70,8 +70,10 @@ export class LoginByPhonePage {
   login() {
     this.httpService.login(this.loginInfo).then(data => {
       if (data.status == 1) {
-        this.httpService.setToken(data.data.token);
-        this.httpService.setStorage(this.httpService.HAS_LOGGED_IN, true);
+        this.httpService.setStorage('token', data.data.token);
+        this.httpService.setStorage('hasLoggedIn', true);
+        this.httpService.setStorage('username', data.data.username);
+        this.httpService.setStorage('login_info', data);
 
         let toast = this.toastCtrl.create({
           message: "欢迎回来，" + data.data.user_name || this.loginInfo.userphone,
