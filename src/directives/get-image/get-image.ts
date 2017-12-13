bfa6c2a42fdd689915fef8f0ab72607f09195e83
firebat1314 <1121112856@qq.com>
2017-12-13 20:23:43 +0800

    上传图片，列表默认样式

--- src/directives/get-image/get-image.ts
+++ src/directives/get-image/get-image.ts
@@ -31,18 +31,57 @@ export class GetImageDirective {
     if (!imageType.test(file['type'])) {
       this.native.showToast("请选择图片！");
       return;
-    }else if(file['size']>(3*1024*1024)){
-      this.native.showToast("图片超过限制");
-      return;
     }
     //读取完成
     reader.onload = (e) => {
       //获取图片dom
       // console.log('reader',e)
-      var img_ava = e.target['result'];
-      this.fileChecked.emit(img_ava);
+      this.dealImage(e.target['result'],{
+        quality:0.4
+      },(base)=>{
+        // console.log("压缩后：" + base.length / 1024 + " ");　
+        this.fileChecked.emit(base);
+      })
+      /* if(file['size']>(2*1024*1024)){
+          this.native.showToast("图片超过限制");
+      }else{
+        this.fileChecked.emit(e.target['result']);
+      } */
     };
     reader.readAsDataURL(file);
   }
+  dealImage(path, obj, callback){
+    var img = new Image();
+    img.src = path;
+    img.onload = function(){
+     var that = this;
+     // 默认按比例压缩
+     var w = that['width'],
+      h = that['height'],
+      scale = w / h;
+      w = obj.width || w;
+      h = obj.height || (w / scale);
+     var quality = 0.7;  // 默认图片质量为0.7
+     //生成canvas
+     var canvas = document.createElement('canvas');
+     var ctx = canvas.getContext('2d');
+     // 创建属性节点
+     var anw = document.createAttribute("width");
+     anw.nodeValue = w;
+     var anh = document.createAttribute("height");
+     anh.nodeValue = h;
+     canvas.setAttributeNode(anw);
+     canvas.setAttributeNode(anh); 
+     ctx.drawImage(img, 0, 0, w, h);
+     // 图像质量
+     if(obj.quality && obj.quality <= 1 && obj.quality > 0){
+      quality = obj.quality;
+     }
+     // quality值越小，所绘制出的图像越模糊
+     var base64 = canvas.toDataURL('image/jpeg', quality );
+     // 回调函数返回base64的值
+     callback(base64);
+    }
+   }
 
 }
