ec1c6de3970b04d144e632f54e4503e2a939f6eb
firebat1314 <1121112856@qq.com>
2017-12-29 17:27:16 +0800

    注册图片质量

--- src/pages/signup/signup-second/signup-second.module.ts
+++ src/pages/signup/signup-second/signup-second.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SignupSecondPage } from "./signup-second";
 import { GetImageDirectiveModule } from "../../../directives/get-image/get-image.module";
+import { ImgLazyLoadDirectiveModule } from '../../../directives/img-lazy-load/img-lazy-load.module';
 
 @NgModule({
   declarations: [SignupSecondPage],
   imports: [
     IonicPageModule.forChild(SignupSecondPage),
-    GetImageDirectiveModule
+    GetImageDirectiveModule,
+    ImgLazyLoadDirectiveModule,
   ]
 })
 
