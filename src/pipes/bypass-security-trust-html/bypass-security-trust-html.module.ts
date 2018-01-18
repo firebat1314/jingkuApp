b5f910819ffe015e8ac61c003a6f50c8094d81ec
firebat1314 <1121112856@qq.com>
2018-01-18 14:32:50 +0800

    修复手机商品详情样式加不上

---
+++ src/pipes/bypass-security-trust-html/bypass-security-trust-html.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { BypassSecurityTrustHtmlPipe } from './bypass-security-trust-html';
+
+@NgModule({
+  declarations: [BypassSecurityTrustHtmlPipe],
+  exports: [BypassSecurityTrustHtmlPipe]
+})
+export class BypassSecurityTrustHtmlPipeModule { }
--- src/providers/constants.ts
+++ src/providers/constants.ts
@@ -1,5 +1,5 @@
 
-export const APK_DOWNLOAD = 'https://download.fir.im/apps/59fe07a4ca87a82a2e00005d/install?download_token=2462219f7445aa4cf5a695163302d08e&release_id=5a15546f548b7a0b210005a2';
+export const APK_DOWNLOAD = 'https://download.fir.im/apps/59fe07a4ca87a82a2e00005d/install?download_token=2462219f7445aa4cf5a695163302d08e&release_id=5a5de738548b7a306f000595';
 
 export const IOS_DOWNLOAD = 'https://itunes.apple.com/cn/app/%E9%95%9C%E5%BA%93/id1265021927?mt=8';
 
@@ -8,8 +8,8 @@ export const IOS_DOWNLOAD = 'https://itunes.apple.com/cn/app/%E9%95%9C%E5%BA%93/
 // export const IP = 'http://newpc.jingkoo.net';//pc站
 
 
-export const IP = 'http://app.jingku.cn';//app
-// export const IP = 'http://newapp.jingkoo.net';//app测试
+// export const IP = 'http://app.jingku.cn';//app
+export const IP = 'http://newapp.jingkoo.net';//app测试
 // export const IP = 'https://m.jingku.cn';//m站
 // export const IP = 'http://newm.jingkoo.net';//m站测试
 
