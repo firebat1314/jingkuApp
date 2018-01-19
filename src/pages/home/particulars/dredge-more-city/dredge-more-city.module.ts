8b50f087e2a4a1957338caf26b779177aa322a3d
firebat1314 <1121112856@qq.com>
2018-01-19 16:16:25 +0800

    银行开户许可证

--- src/pages/home/particulars/dredge-more-city/dredge-more-city.module.ts
+++ src/pages/home/particulars/dredge-more-city/dredge-more-city.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { DredgeMoreCityPage } from "./dredge-more-city";
-
+import { GetImageDirectiveModule } from '../../../../directives/get-image/get-image.module';
 @NgModule({
    declarations: [
       DredgeMoreCityPage,
+      GetImageDirectiveModule
    ],
    imports: [
       IonicPageModule.forChild(DredgeMoreCityPage),
