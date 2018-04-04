b78537fb0da5e1af30a0583740ef744dc0c3c0f2
firebat1314 <1121112856@qq.com>
2018-04-04 14:41:56 +0800

    add prefix pipe

---
+++ src/pipes/prefix/prefix.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { PrefixPipe } from "./prefix";
+
+@NgModule({
+  declarations: [PrefixPipe],
+  exports: [PrefixPipe]
+})
+export class PrefixPipeModule { }
