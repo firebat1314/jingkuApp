2ba91eb12b847942218962bf420120b129243f87
firebat1314 <1121112856@qq.com>
2018-07-29 21:25:27 +0800

    搜索历史，活动商品

--- src/pages/home/discount-coupon/discount-coupon.ts
+++ src/pages/home/discount-coupon/discount-coupon.ts
@@ -11,113 +11,87 @@ import { Native } from "../../../providers/native";
 */
 @IonicPage()
 @Component({
-  selector: 'page-discount-coupon',
-  templateUrl: 'discount-coupon.html'
+   selector: 'page-discount-coupon',
+   templateUrl: 'discount-coupon.html'
 })
 export class DiscountCouponPage {
-  data: any;
-  @ViewChild(Content) content: Content;
-  @ViewChild(FabButton) fabButton: FabButton;
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public native: Native,
-    public events: Events,
-    public httpService: HttpService
-  ) { }
+   data: any;
+   @ViewChild(Content) content: Content;
+   @ViewChild(FabButton) fabButton: FabButton;
+   constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      public native: Native,
+      public events: Events,
+      public httpService: HttpService
+   ) { }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DiscountCouponPage');
-  }
-  ngOnInit(){
-    this.getCouponData();
-  }
-  ngAfterViewInit() {
-    /* 回到顶部按钮 */
-    this.fabButton.setElementClass('fab-button-out',true);
-    this.content.ionScroll.subscribe((d) => {
-      this.fabButton.setElementClass("fab-button-in", d.scrollTop >= d.contentHeight);
-    });
-  }
-  getCouponData() {
-    this.httpService.coupon({ page: 1 }).then((res) => {
-      if (res.status == 1) {
-        this.data = res;
+   ionViewDidLoad() {
+      console.log('ionViewDidLoad DiscountCouponPage');
+   }
+   ngOnInit() {
+      this.getCouponData();
+   }
+   ngAfterViewInit() {
+      /* 回到顶部按钮 */
+      this.fabButton.setElementClass('fab-button-out', true);
+      this.content.ionScroll.subscribe((d) => {
+         this.fabButton.setElementClass("fab-button-in", d.scrollTop >= d.contentHeight);
+      });
+   }
+   getCouponData() {
+      this.httpService.coupon({ page: 1 }).then((res) => {
+         if (res.status == 1) {
+            this.data = res;
+         }
+      })
+   }
+   getPrivilege(is_get, type_id, suppliers_id) {
+      if (is_get == 1 || is_get == 2) {
+         if (suppliers_id == 0) {
+            this.navCtrl.push('BrandListPage', { keyword: '镜库' })
+         } else if (suppliers_id < 0) {
+            this.navCtrl.push('BrandListPage', { keyword: '' })
+         } else {
+            this.navCtrl.push('ParticularsHomePage', { suppliersId: suppliers_id })
+         }
+      }else if (is_get == 0) {
+         this.native.openAlertBox('确认领取优惠券', () => {
+            this.httpService.sendByUser({ type_id: type_id }).then((res) => {
+               if (res.status == 1) {
+                  this.native.showToast('领取优惠券成功');
+                  is_get = 1
+                  this.getCouponData();
+               }
+            })
+         })
       }
-    })
-  }
-  getPrivilege(is_get, type_id,suppliers_id) {
-    if (is_get == 1||is_get==2) {
-      this.navCtrl.push('ParticularsHomePage',{suppliersId:suppliers_id})
-    }
-    if (is_get == 0) {
-      this.native.openAlertBox('确认领取优惠券', () => {
-        this.httpService.sendByUser({ type_id: type_id }).then((res) => {
-          if (res.status == 1) {
-            this.native.showToast('领取优惠券成功');
-            is_get = 1
-            this.getCouponData();
-          }
-        })
-      })
-    }
-  }
-  goParticularsHomePage(suppliers_id){
-      this.navCtrl.push('ParticularsHomePage',{suppliersId:suppliers_id})
-  }
-  goClassPage(value) {
-    this.navCtrl.pop().catch(res => { history.back() });
-    this.navCtrl.parent.select(1);
-    this.events.publish('classify:selectSegment', value);
-  }
+   }
+   goParticularsHomePage(suppliers_id) {
+      this.navCtrl.push('ParticularsHomePage', { suppliersId: suppliers_id })
+   }
+   goClassPage(value) {
+      this.navCtrl.pop().catch(res => { history.back() });
+      this.navCtrl.parent.select(1);
+      this.events.publish('classify:selectSegment', value);
+   }
 
-  flag: boolean = true;
-  doInfinite(infiniteScroll) {
-    if (this.data.page < this.data.pages) {
-      this.httpService.coupon({ page: ++this.data.page }).then((res) => {
-        if (res.status == 1) {
-          Array.prototype.push.apply(this.data.list, res.list);
-        }
-        setTimeout(() => {
-          infiniteScroll.complete();
-        }, 500);
-      })
-    } else {
-      this.flag = false;
-    }
-  }
-  scrollToTop() {
-    this.content.scrollToTop();
-  }
+   flag: boolean = true;
+   doInfinite(infiniteScroll) {
+      if (this.data.page < this.data.pages) {
+         this.httpService.coupon({ page: ++this.data.page }).then((res) => {
+            if (res.status == 1) {
+               Array.prototype.push.apply(this.data.list, res.list);
+            }
+            setTimeout(() => {
+               infiniteScroll.complete();
+            }, 500);
+         })
+      } else {
+         this.flag = false;
+      }
+   }
+   scrollToTop() {
+      this.content.scrollToTop();
+   }
 }
-// export class Ring {
-//   percent: any;
-//   startAngle: any;
-//   radius = 28;    // 圆环半径
-//   lineWidth = 5;  // 圆环边的宽度
-//   strokeStyle = '#ccc'; //边的颜色
-//   fillStyle = '#fff';  //填充色
-//   lineCap = 'round';
-//   constructor(startAngle, percent) {
-//     this.startAngle = startAngle || 3 * Math.PI / 2; //弧起始角度
-//     this.percent = percent
-//   }
-//   draw = (ctx) => {
-//     ctx.beginPath();
-//     ctx.arc(30, 45, this.radius, 0, Math.PI * 2, true);  // 坐标为250的圆，这里起始角度是0，结束角度是Math.PI*2
-//     ctx.lineWidth = this.lineWidth;
-//     ctx.strokeStyle = this.strokeStyle;
-//     ctx.stroke();  // 这里用stroke画一个空心圆，想填充颜色的童鞋可以用fill方法
-//   };
-//   drawRing = (ctx) => {
-//     this.draw(ctx);  // 调用Circle的draw方法画圈圈
-//     ctx.beginPath();
-//     var anglePerSec = 2 * Math.PI / (100 / this.percent); // 蓝色的弧度
-//     ctx.arc(35, 45, this.radius, this.startAngle, this.startAngle, false); //这里的圆心坐标要和cirle的保持一致
-//     ctx.strokeStyle = this.fillStyle;
-//     ctx.lineCap = this.lineCap;
-//     ctx.stroke();
-//     ctx.closePath();
-//   }
-
-// }
--- src/pages/home/fastbuy/fastbuy.ts
+++ src/pages/home/fastbuy/fastbuy.ts
@@ -11,72 +11,72 @@ import { MineProvider } from '../../../providers/mine/mine';
 */
 @IonicPage()
 @Component({
-  selector: 'page-fastbuy',
-  templateUrl: 'fastbuy.html'
+   selector: 'page-fastbuy',
+   templateUrl: 'fastbuy.html'
 })
 export class FastbuyPage {
-  infiniteScroll: any;
-  size: any = 10;
+   infiniteScroll: any;
+   size: any = 10;
 
-  category: any;
-  data: any;
-  selected = 0;
-  page = 1;
-  @ViewChild(Content) content: Content;
-  @ViewChild(FabButton) fabButton: FabButton;
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    private httpService: HttpService,
-    private elementRef: ElementRef,
-    private mine: MineProvider,
-  ) { }
-  ngOnInit() {
-    this.httpService.getCategoryPromote().then((data) => {
-      if (data.status) {
-        this.category = data;
-      }
-    })
-    this.getData(this.selected);
-  }
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FastbuyPage');
-  }
-  ngAfterViewInit() {
-    /* 回到顶部按钮 */
-    this.fabButton.setElementClass('fab-button-out', true);
-    this.content.ionScroll.subscribe((d) => {
-      this.fabButton.setElementClass("fab-button-in", d.scrollTop >= d.contentHeight);
-    });
-  }
-  getData(id) {
-    this.page = 1;
+   category: any;
+   data: any;
+   selected = 0;
+   page = 1;
+   @ViewChild(Content) content: Content;
+   @ViewChild(FabButton) fabButton: FabButton;
+   constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      private httpService: HttpService,
+      private elementRef: ElementRef,
+      private mine: MineProvider,
+   ) { }
+   ngOnInit() {
+      this.httpService.getCategoryPromote().then((data) => {
+         if (data.status) {
+            this.category = data;
+         }
+      })
+      this.getData(this.selected);
+   }
+   ionViewDidLoad() {
+      console.log('ionViewDidLoad FastbuyPage');
+   }
+   ngAfterViewInit() {
+      /* 回到顶部按钮 */
+      this.fabButton.setElementClass('fab-button-out', true);
+      this.content.ionScroll.subscribe((d) => {
+         this.fabButton.setElementClass("fab-button-in", d.scrollTop >= d.contentHeight);
+      });
+   }
+   getData(id) {
+      this.page = 1;
 
-    this.selected = id;
-    this.infiniteScroll ? this.infiniteScroll.enable(true) : null;
+      this.selected = id;
+      this.infiniteScroll ? this.infiniteScroll.enable(true) : null;
 
-    this.httpService.presell({ page: this.page, num: this.size, type: 'is_promote', cat_id: id }).then((res) => {
-      if (res.status == 1) { this.data = res; }
-    })
-  }
-  goParticularsPage(id) {
-    this.navCtrl.push('ParticularsPage', { goodsId: id })
-  }
-  scrollToTop() {
-    this.content.scrollToTop();
-  }
-  doInfinite(infiniteScroll) {
-    this.infiniteScroll = infiniteScroll;
-    this.httpService.presell({ num: this.size, page: ++this.page, type: 'is_promote', cat_id: this.selected }, { showLoading: false }).then((res) => {
-      if (res.status == 1) {
-        if (!res.data.length) {
-          this.infiniteScroll.enable(false);
-        }
-        this.data.data = this.data.data.concat(res.data);
-      }
-      setTimeout(() => {
-        this.infiniteScroll.complete();
-      }, 500);
-    })
-  }
+      this.httpService.presell({ page: this.page, num: this.size, type: 'is_promote', cat_id: id }).then((res) => {
+         if (res.status == 1) { this.data = res; }
+      })
+   }
+   goParticularsPage(id) {
+      this.navCtrl.push('ParticularsPage', { goodsId: id, isActivity: 1 })
+   }
+   scrollToTop() {
+      this.content.scrollToTop();
+   }
+   doInfinite(infiniteScroll) {
+      this.infiniteScroll = infiniteScroll;
+      this.httpService.presell({ num: this.size, page: ++this.page, type: 'is_promote', cat_id: this.selected }, { showLoading: false }).then((res) => {
+         if (res.status == 1) {
+            if (!res.data.length) {
+               this.infiniteScroll.enable(false);
+            }
+            this.data.data = this.data.data.concat(res.data);
+         }
+         setTimeout(() => {
+            this.infiniteScroll.complete();
+         }, 500);
+      })
+   }
 }
