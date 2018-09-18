57c0f71706ff1191e8a4d240504eaf0327604143
firebat1314 <1121112856@qq.com>
2018-09-18 15:36:33 +0800

    闪购改版

--- src/directives/get-image/get-image.ts
+++ src/directives/get-image/get-image.ts
@@ -9,109 +9,127 @@ import { ActionSheetController } from 'ionic-angular';
  * for more info on Angular Directives.
  */
 @Directive({
-	selector: '[get-image]' // Attribute selector
+    selector: '[get-image]' // Attribute selector
 })
 export class GetImageDirective {
 
-	constructor(
-		private native: Native,
-		public actionSheetCtrl: ActionSheetController,
-	) {
-		console.log('Hello GetImageDirective Directive');
-	}
-	@Output('get-image') fileChecked: EventEmitter<string> = new EventEmitter<string>();
-	@Input() quality: number;
-	@Input() CameraOptions: any;
+    constructor(
+        private native: Native,
+        public actionSheetCtrl: ActionSheetController,
+    ) {
+        console.log('Hello GetImageDirective Directive');
+    }
+    @Output('get-image') fileChecked: EventEmitter<string> = new EventEmitter<string>();
+    @Input() quality: number;
+    @Input() CameraOptions: any;
 
-	@HostListener('click', ['$event']) onclick(e) {
-		if (this.native.isMobile()) {
-			e.preventDefault()
-			let actionSheet = this.actionSheetCtrl.create({
-				buttons: [
-					{
-						text: '拍照上传',
-						role: 'destructive',
-						handler: () => {
-							this.native.getPictureByCamera(this.CameraOptions).then((data) => {
-								this.fileChecked.emit('data:image/jpg;base64,' + data);
-							})
-						}
-					},
-					{
-						text: '本地上传',
-						handler: () => {
-							this.native.getPictureByPhotoLibrary(this.CameraOptions).then((data) => {
-								this.fileChecked.emit('data:image/jpg;base64,' + data);
-							})
-						}
-					},
-					{
-						text: '取消',
-						role: 'cancel',
-						handler: () => { }
-					}
-				]
-			});
-			actionSheet.present();
-		}
-	}
-	@HostListener('change', ['$event']) onchange(e) {
-		if (!e) { return; }
-		let file = e.target.files[0];//获取文件
-		let imageType = /^image\//;
-		if (!imageType.test(file.type)) {//判断图片
-			this.native.showToast("请选择图片！");
-			return;
-		}
-		let reader = new FileReader();
-		reader.readAsDataURL(file);
-		reader.onload = (event) => {//读取完成
-			console.log(reader, event)
-			if (file.size > (6 * 1024 * 1024)) {
-				this.native.showToast("图片超过限制");
-			} else {
-				console.log('压缩品质:', this.quality);
-				this.dealImage(event.target['result'], (base) => {
-					// console.log("压缩后：" + base.length / 1024 + " ");　
-					this.fileChecked.emit(base);
-				}, {
-						quality: this.quality
-					})
-			}
-		};
-		// console.log(reader, reader.onload, reader.onloadend, reader.readAsDataURL)
-	}
-	dealImage(path: string, callback, obj: { width?: number, height?: number, quality?: number } = {}) {
-		let img = new Image();
-		img.src = path;
-		img.onload = function () {
-			// 默认按比例压缩
-			let w = img.width,
-				h = img.height,
-				scale = w / h;
-			w = obj.width || w;
-			h = obj.height || (w / scale);
-			let quality = 0.9;  // 默认图片质量为0.7
-			//生成canvas
-			let canvas = document.createElement('canvas');
-			let ctx = canvas.getContext('2d');
-			// 创建属性节点
-			let anw = document.createAttribute("width");
-			anw.nodeValue = String(w);
-			let anh = document.createAttribute("height");
-			anh.nodeValue = String(h);
-			canvas.setAttributeNode(anw);
-			canvas.setAttributeNode(anh);
-			ctx.drawImage(img, 0, 0, w, h);
-			// 图像质量
-			if (obj.quality && obj.quality <= 1 && obj.quality > 0) {
-				quality = 0.9;
-			}
-			// quality值越小，所绘制出的图像越模糊
-			let base64 = canvas.toDataURL('image/jpeg', quality);
-			// 回调函数返回base64的值
-			callback(base64);
-		}
-	}
+    @HostListener('click', ['$event']) onclick(e) {
+        if (this.native.isMobile()) {
+            e.preventDefault()
+            let actionSheet = this.actionSheetCtrl.create({
+                buttons: [
+                    {
+                        text: '拍照上传',
+                        role: 'destructive',
+                        handler: () => {
+                            this.native.getPictureByCamera(this.CameraOptions).then((data) => {
+                                this.fileChecked.emit('data:image/jpg;base64,' + data);
+                            })
+                        }
+                    },
+                    {
+                        text: '本地上传',
+                        handler: () => {
+                            this.native.getPictureByPhotoLibrary(this.CameraOptions).then((data) => {
+                                this.fileChecked.emit('data:image/jpg;base64,' + data);
+                            })
+                        }
+                    },
+                    {
+                        text: '取消',
+                        role: 'cancel',
+                        handler: () => { }
+                    }
+                ]
+            });
+            actionSheet.present();
+        }
+    }
+    @HostListener('change', ['$event']) onchange(e) {
+        if (!e) { return; }
+        let file = e.target.files[0];//获取文件
+        let imageType = /^image\//;
+        if (!imageType.test(file.type)) {//判断图片
+            this.native.showToast("请选择图片！");
+            return;
+        }
+        if (file.size > (10 * 1024 * 1024)) {
+            this.native.showToast("图片超过限制");
+        } else {
+            this.fileChecked.emit(file);
+
+            /* let reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = (event) => {//读取完成
+                console.log('压缩品质:', this.quality);
+                this.dealImage(event.target['result'], (base) => {
+                    // console.log("压缩后：" + base.length / 1024 + " ");　
+                    this.fileChecked.emit(base);
+                }, {
+                        quality: this.quality
+                    })
+            }; */
+            // console.log(reader, reader.onload, reader.onloadend, reader.readAsDataURL)
+        }
+    }
+    //上传 
+    /* public submitUploadFile() {
+        if (this.fileList.length > 0) {
+            let file: File = this.fileList[0];
+            let formData = new FormData();
+            formData.append('file', file, file.name);
+            let headers = new Headers();
+            headers.append('Accept', 'application/json');
+            let options = new RequestOptions({ headers: headers });
+            this.http.post(url, formData, options)
+                .map(res => res.json())
+                .catch(error => Observable.throw(error))
+                .subscribe(
+
+                )
+        }
+    } */
+    dealImage(path: string, callback, obj: { width?: number, height?: number, quality?: number } = {}) {
+        let img = new Image();
+        img.src = path;
+        img.onload = function() {
+            // 默认按比例压缩
+            let w = img.width,
+                h = img.height,
+                scale = w / h;
+            w = obj.width || w;
+            h = obj.height || (w / scale);
+            let quality = 0.9;  // 默认图片质量为0.7
+            //生成canvas
+            let canvas = document.createElement('canvas');
+            let ctx = canvas.getContext('2d');
+            // 创建属性节点
+            let anw = document.createAttribute("width");
+            anw.nodeValue = String(w);
+            let anh = document.createAttribute("height");
+            anh.nodeValue = String(h);
+            canvas.setAttributeNode(anw);
+            canvas.setAttributeNode(anh);
+            ctx.drawImage(img, 0, 0, w, h);
+            // 图像质量
+            if (obj.quality && obj.quality <= 1 && obj.quality > 0) {
+                quality = 0.9;
+            }
+            // quality值越小，所绘制出的图像越模糊
+            let base64 = canvas.toDataURL('image/jpeg', quality);
+            // 回调函数返回base64的值
+            callback(base64);
+        }
+    }
 
 }
--- src/pages/home/distribution/distribution.module.ts
+++ src/pages/home/distribution/distribution.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { FastbuyPage } from "./fastbuy";
-import { CountdownComponentModule } from "../../../components/countdown/countdown.module";
 import { ImgLazyLoadDirectiveModule } from "../../../directives/img-lazy-load/img-lazy-load.module";
 import { NoGoodsComponentModule } from "../../../components/no-goods/no-goods.module";
+import { DistributionPage } from './distribution';
 
 @NgModule({
-  declarations: [FastbuyPage],
+  declarations: [DistributionPage],
   imports: [
-    IonicPageModule.forChild(FastbuyPage),
-    CountdownComponentModule,
+    IonicPageModule.forChild(DistributionPage),
     ImgLazyLoadDirectiveModule,
     NoGoodsComponentModule,
   ]
 })
 
-export class FastbuyPageModule { }
+export class DistributionPageModule { }
