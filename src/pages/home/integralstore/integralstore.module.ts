c15ea9156024c1bc5b53d686e0aa0c40de241b65
firebat1314 <1121112856@qq.com>
2018-10-24 09:32:27 +0800

    专区图片改广告位,加工眼镜片样式，业务员判断

--- src/pages/home/integralstore/integralstore.module.ts
+++ src/pages/home/integralstore/integralstore.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { IntegralstorePage } from "./integralstore";
 import { ImgLazyLoadDirectiveModule } from "../../../directives/img-lazy-load/img-lazy-load.module";
+import { AdsClickDirectiveModule } from '../../../directives/ads-click/ads-click.module';
 
 @NgModule({
    declarations: [IntegralstorePage],
    imports: [
       IonicPageModule.forChild(IntegralstorePage),
-      ImgLazyLoadDirectiveModule
+      ImgLazyLoadDirectiveModule,
+      AdsClickDirectiveModule
    ]
 })
 
